ce40da37601e0586dd7df8193cea43947d5c142b
Itokawa-Naoki <87311405+Itokawa-Naoki@users.noreply.github.com>
2021-10-09 17:00:48 +0900

    Update 1_FPKM_calculation.sh

--- RNAseq/1_FPKM_calculation.sh
+++ RNAseq/1_FPKM_calculation.sh
@@ -6,15 +6,11 @@ fastq-dump $exapmle
 # get mm10 annotation file
 wget http://igenomes.illumina.com.s3-website-us-east-1.amazonaws.com/Mus_musculus/UCSC/mm10/Mus_musculus_UCSC_mm10.tar.gz
 tar -zxvf Mus_musculus_UCSC_mm10.tar.gz
-bowtie2-build -f Mus_musculus/UCSC/mm10/Sequence/WholeGenomeFasta/genome.fa mm10_index
-
-# get index for tophat2(bowtie)
-wget "ftp://ftp.ccb.jhu.edu/pub/data/bowtie2_indexes/mm10.zip"
-mkdir indexes
-unzip mm10.zip -d ./indexes/
 
 # Mapping to coding region
-tophat2 -r 100 -p 1 -G Mus_musculus/UCSC/mm10/Annotation/Archives/archive-2015-07-17-14-33-26/Genes/genes.gtf mm10_index "${example}.fastq"
+tophat2 -r 100 -p 1 -G Mus_musculus/UCSC/mm10/Annotation/Archives/archive-2015-07-17-14-33-26/Genes/genes.gtf ¥
+Mus_musculus/UCSC/mm10/Sequence/Bowtie2Index/genome ¥
+"${example}.fastq"
 
 # bam sorting and indexing
 samtools sort -O bam -o sort_accepted_hits.bam accepted_hits.bam 
