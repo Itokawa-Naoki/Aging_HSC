efc9acffd95d62ceb374ce18728ecf6c8ce39a7e
Itokawa-Naoki <87311405+Itokawa-Naoki@users.noreply.github.com>
2021-10-09 17:18:59 +0900

    Update 1_FPKM_calculation.sh

--- RNAseq/1_FPKM_calculation.sh
+++ RNAseq/1_FPKM_calculation.sh
@@ -3,18 +3,18 @@
 example="SRR13192285"
 fastq-dump $exapmle	
 
-# get mm10 annotation file
+# get mm10 reference and annotation file
 wget http://igenomes.illumina.com.s3-website-us-east-1.amazonaws.com/Mus_musculus/UCSC/mm10/Mus_musculus_UCSC_mm10.tar.gz
 tar -zxvf Mus_musculus_UCSC_mm10.tar.gz
 
 # Mapping to coding region
-tophat2 -r 100 -p 1 -G Mus_musculus/UCSC/mm10/Annotation/Archives/archive-2015-07-17-14-33-26/Genes/genes.gtf ¥
-Mus_musculus/UCSC/mm10/Sequence/Bowtie2Index/genome ¥
+tophat2 -r 100 -p 1 -G Mus_musculus/UCSC/mm10/Annotation/Archives/archive-2015-07-17-14-33-26/Genes/genes.gtf \
+Mus_musculus/UCSC/mm10/Sequence/Bowtie2Index/genome \
 "${example}.fastq"
 
 # bam sorting and indexing
 samtools sort -O bam -o sort_accepted_hits.bam accepted_hits.bam 
 samtools index sort_accepted_hits.bam
 
-# calculation of FPKM and read count for DESeq2 by stringtie
+# calculation of raw read count for DESeq2 and FPKM by stringtie
 stringtie -e -B -p 16 -G Mus_musculus/UCSC/mm10/Annotation/Archives/archive-2015-07-17-14-33-26/Genes/genes.gtf -o ${example}.gtf -A ${example}_abd.txt sort_accepted_hits.bam
