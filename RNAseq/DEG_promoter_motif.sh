3afc3bcca4bff6c5e4bb0e84469e71f7511e5126
Itokawa-Naoki <87311405+Itokawa-Naoki@users.noreply.github.com>
2021-10-09 23:09:09 +0900

    Update DEG_promoter_motif.sh

--- RNAseq/DEG_promoter_motif.sh
+++ RNAseq/DEG_promoter_motif.sh
@@ -1,1 +1,3 @@
-findMotifs.pl ${DEG} mouse promoter_motif_${file}
+wget https://github.com/Itokawa-Naoki/Aging_HSC/raw/main/data/AgedHSC_up_DEG.txt
+
+findMotifs.pl AgedHSC_up_DEG.txt mouse promoter_motif_AgedHSC_up_DEG.txt
